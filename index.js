4ada06a962806c5e05f6a2c120997beef884bae0
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-02-22 02:07:22 +0600

    video 5 down

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 // middleware 
@@ -41,6 +41,13 @@ async function run() {
         const user = req.body;
         const result = await userCollection.insertOne(user);
         res.send(result);
+    })
+
+    app.delete("/users/:id", async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await userCollection.deleteOne(query)
+        res.send(result);        
     })
 
 
